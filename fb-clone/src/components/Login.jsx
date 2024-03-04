947f91cc4d1af24ff5091cab0043a30bef8d4b85
Kaveenxz <kaveenhansithx@gmail.com>
2024-03-04 12:30:38 +0530

    make sign up

--- fb-clone/src/components/Login.jsx
+++ fb-clone/src/components/Login.jsx
@@ -1,6 +1,8 @@
 import './login.css'
+import Signin from './Signin'
 
 export default function Login() {
+
 
     return (
         <div className="container mt-20">
@@ -27,12 +29,100 @@ export default function Login() {
                             </div>
                             <hr />
                             <div className="text-center">
-                                <button className="btn btn-success w-90 py-2 fw-bold mt-2 mb-1 bg-custom-color-2" type="submit">Create new account</button>
+                                <button className="btn btn-success w-90 py-2 fw-bold mt-2 mb-1 bg-custom-color-2" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button">Create new account</button>
                             </div>
                         </form>
                         <p className="mt-3 m-4 text-black"><a href="" className="text-decoration-none text-black fw-bold">Create a Page</a> for a celebrity, brand or business.</p>
 
                     </main>
+                </div>
+            </div>
+
+            <div className="modal fade mt-3" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-2 fw-bold" id="exampleModalLabel">Sign Up</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+
+                        <div className="modal-body">
+                            <form class="needs-validation" novalidate>
+                                <div class="row g-3">
+
+
+                                    <div class="col-sm-6">
+                                        <input type="text" class="form-control" id="firstName" placeholder="First name" value="" required />
+                                        <div class="invalid-feedback">
+                                            Valid first name is required.
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm-6">
+                                        <input type="text" class="form-control" id="lastName" placeholder="Surname" value="" required />
+                                        <div class="invalid-feedback">
+                                            Valid last name is required.
+                                        </div>
+                                    </div>
+
+                                    <div class="col-12">
+                                        <div class="input-group has-validation">
+                                            <input type="text" class="form-control" id="username" placeholder="Mobile number or email address" required />
+                                            <div class="invalid-feedback">
+                                                Your username is required.
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-12">
+                                        <input type="text" class="form-control" id="email" placeholder="New password" />
+                                        <div class="invalid-feedback">
+                                            Please enter a valid email address for shipping updates.
+                                        </div>
+                                    </div>
+
+                                    <small>Date of Birth</small>
+                                    <div class="col-12">
+                                        <input type="date" class="form-control" id="firstName" value="" placeholder='date' required />
+                                    </div>
+
+                                    <div class="row g-3">
+                                    <small >Gender</small>
+                                        <div class="col-sm-4">
+                                            <label class="radio-label">
+                                            Male
+                                                <input type="radio" class="form-check-input" name="gender" value="male" required />
+                                                
+                                            </label>
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <label class="radio-label">
+                                            Female
+                                                <input type="radio" class="form-check-input" name="gender" value="female" required />
+                                                
+                                            </label>
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <label class="radio-label">
+                                                Custom
+                                                <input type="radio" class="form-check-input" name="gender" value="other" required />
+                                                
+                                            </label>
+                                        </div>
+                                    </div>
+
+
+
+
+                                </div>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <small>People who use our service may have uploaded your contact information to Facebook. Learn more.</small>
+                            <small>By clicking Sign Up, you agree to our Terms, Privacy Policy and Cookies Policy. You may receive SMS notifications from us and can opt out at any time.</small>
+                            <button class="btn btn-success custom-btn w-100 py-2 fw-bold mt-2 mb-1" type="submit">Sign up</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- fb-clone/src/main.jsx
+++ fb-clone/src/main.jsx
@@ -3,5 +3,6 @@ import { App } from './app.jsx'
 import './index.css'
 import './components/login.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
 
 render(<App />, document.getElementById('app'))
