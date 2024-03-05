0f1e37ee3a64c85b670728e7406b731f0117ac7e
Kaveenxz <kaveenhansithx@gmail.com>
2024-03-05 11:40:43 +0530

    make navbar

--- fb-clone/src/home/navbar.jsx
+++ fb-clone/src/home/navbar.jsx
@@ -1,5 +1,13 @@
 import logo from '../assets/logo.png'
-
+import home from '../assets/home.png'
+import group from '../assets/group.png'
+import game from '../assets/game.png'
+import video from '../assets/video.png'
+import bell from '../assets/bell.png'
+import menu from '../assets/menu.png'
+import profile from '../assets/profile.jpg'
+import massenger from '../assets/massenger.png'
+import './home.css'
 export default function Navbar() {
     return (
         <nav class="navbar navbar-expand-lg bg-body-tertiary rounded" aria-label="Thirteenth navbar example">
@@ -15,33 +23,42 @@ export default function Navbar() {
                         </div>
                         <div className="col-sm-10 mt-2">
                             <form role="search">
-                                <input class="form-control" type="search" placeholder="Search facebook" aria-label="Search" style={'border-radius:18px'}/>
+                                <input class="form-control" type="search" placeholder="Search facebook" aria-label="Search" style={'border-radius:18px'} />
                             </form>
                         </div>
                     </div>
 
-                    <ul class="navbar-nav col-lg-6 justify-content-lg-center">
+                    <ul class="navbar-nav col-lg-6 justify-content-lg-center ms-5">
 
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <li class="nav-item me-5 ms-5">
+                            <a class="nav-link active" aria-current="page" href="#"><img src={home} alt="" style={'width:30px'} /></a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                        <li class="nav-item me-5 ms-5">
+                            <a class="nav-link active" aria-current="page" href="#"><img src={video} alt="" style={'width:30px'} /></a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                        <li class="nav-item me-5 ms-5">
+                            <a class="nav-link active" aria-current="page" href="#"><img src={group} alt="" style={'width:30px'} /></a>
                         </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                        <li class="nav-item me-5 ms-5">
+                            <a class="nav-link active" aria-current="page" href="#"><img src={game} alt="" style={'width:30px'} /></a>
                         </li>
                     </ul>
                     <div class="d-lg-flex col-lg-3 justify-content-lg-end">
-                        <button class="btn btn-primary">Button</button>
+                        <ul class="navbar-nav col-lg-6 justify-content-lg-center" id="profile-list">
+
+                            <li class="nav-item ">
+                                <a class="nav-link active" aria-current="page" href="#"><img src={menu} alt="" style={'width:25px'} /></a>
+                            </li>
+                            <li class="nav-item  ">
+                                <a class="nav-link active" aria-current="page" href="#"><img src={massenger} alt="" style={'width:25px'} /></a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link active" aria-current="page" href="#"><img src={bell} alt="" style={'width:25px'} /></a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link active" aria-current="page" href="#"><img src={profile} alt="" style={'width:25px'} /></a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
